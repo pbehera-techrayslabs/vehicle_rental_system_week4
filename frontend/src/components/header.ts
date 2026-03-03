3056a98fb786cffcfca6e2cae1c460d32d7b14e4
pbehera-techrayslabs <pbehera@techrayslabs.com>
2026-03-03 12:12:48 +0530

    fixed index path in header.ts

--- frontend/src/components/header.ts
+++ frontend/src/components/header.ts
@@ -3,7 +3,7 @@ class Myheader extends HTMLElement {
     this.innerHTML = `
       <header class="bg-blue-600 text-white p-6 shadow-md flex items-center justify-between">
         <h1 class="text-3xl font-bold grow">Vehicle Rental System</h1>
-        <a href="/frontend/homepage/index.html" 
+        <a href="/frontend/index.html" 
            class="ml-4 bg-blue-800 hover:bg-blue-900 text-white px-4 py-2 rounded font-semibold">
           Home
         </a>
